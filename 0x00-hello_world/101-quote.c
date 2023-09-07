c543f00c15695064967423b55a58965db786e7a1
Ziyanda <zeenkosi572@gmail.com>
2023-09-07 11:37:19 -0700

    Prints a quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,11 @@
 
 int main(void)
 {
-    const char *message =
-        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int s = strlen(message);
+	const char *message =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int s = strlen(message);
 
-    write(2, message, s);
-    return (1);
+	write(2, message, s);
+	return (1);
 }
+
