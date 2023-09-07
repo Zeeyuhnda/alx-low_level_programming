cdd6403c8a86a12ade1eca06db72d5d19d2ec249
Ziyanda <zeenkosi572@gmail.com>
2023-09-07 11:38:54 -0700

    Prints a quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,18 @@
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (error code)
+ */
 int main(void)
 {
-	const char *message =
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	int s = strlen(message);
+    const char *message =
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int s = strlen(message);
 
-	write(2, message, s);
-	return (1);
+    write(2, message, s);
+    return (1);
 }
 
