d1361609bd09447a86ab63788a1c8d54b1531175
Ziyanda <zeenkosi572@gmail.com>
2023-09-12 07:36:18 -0700

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include _putchar
+
+int main(void)
+{
+	char _putchar[] = '_putchar'
+		int i = 0;
+
+	while (_putchar[i] != '\0')
+	{
+		_putchar(_putchar[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
