32d8e24f0220a554c9b1e9798e743d689bbdbb03
Ziyanda <zeenkosi572@gmail.com>
2023-09-14 03:12:35 -0700

    Bonus tasks

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * mul - multiply int
+ * mul - multiply 2 int
  * @a - int type
  * @b - int type
  * Return: product of multiplication
  */
 int mul(int a, int b)
 {
-	return (a*b)
+	return (a * b)
 }
