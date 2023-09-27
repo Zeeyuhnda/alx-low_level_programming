e0a17076c5d334718f9cf37eefa4d21a37e0a33f
Ziyanda <zeenkosi572@gmail.com>
2023-09-27 15:53:55 +0200

    Optional taks

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -19,7 +19,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	if (*s1 == *s2)
 	{
-		return wildcmp(++s1, ++s2)
+		return wildcmp(++s1, ++s2);
 	}
 	else if (*s1 == '*')
 	{
@@ -36,7 +36,7 @@ int wildcmp(char *s1, char *s2)
 		       return wildcmp(s1, ++s2);
 		else
 		{
-			return wildcmp(s1 +finsrc(s1, *(s2 + 1), 0, 0), s2;
+			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
 		}
 	}
 	return (0);
