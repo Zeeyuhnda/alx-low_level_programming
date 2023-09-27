d9211acaac5e8e6e59f1b8eb63a1f19df5e1a891
Ziyanda <zeenkosi572@gmail.com>
2023-09-27 15:04:54 +0200

    task 2 - 6

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * _sqrt_recursion - return the natural square root of a number
- *   * @n: int number
- *    * Return: If no natural square root, return -1. Else return natural
- *     * square root
- *      */
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: int number
+ * Return: If no natural square root, return -1. Else return natural
+ * square root
+ */
 
 int _sqrt_recursion(int n)
 {
@@ -12,11 +12,11 @@ int _sqrt_recursion(int n)
 }
 
 /**
- *  * halp - helper function to solve _sqrt_recursion
- *   * @c: number to determine if square root
- *    * @i: incrementer to compare against `c`
- *     * Return: square root if natural square root, or -1 if none found
- *      */
+ * halp - helper function to solve _sqrt_recursion
+ * @c: number to determine if square root
+ * @i: incrementer to compare against `c`
+ * Return: square root if natural square root, or -1 if none found
+ */
 
 int halp(int c, int i)
 {
