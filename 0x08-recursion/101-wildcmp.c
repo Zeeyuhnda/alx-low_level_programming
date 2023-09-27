1e801280c7a63e249817bb7407ce9bf40dc1db63
Ziyanda <zeenkosi572@gmail.com>
2023-09-27 16:02:49 +0200

    Optional taks

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,6 +3,7 @@
  * wildcmp - compares two strings with a 'wildcard expansion' capabilities
  * @s1: string 1
  * @s2: string 2
+ * findsrc - targets wildcards
  * Return: 1 if strings are identical, else 0
  */
 int wildcmp(char *s1, char *s2)
@@ -12,31 +13,31 @@ int wildcmp(char *s1, char *s2)
 	else if (*s1 == '\0' || *s2 == '\0')
 	{
 		if (*s1 == '\0' && *s2 == '*')
-			return wildcmp(s1, ++s2);
+			return (wildcmp(s1, ++s2));
 		else if (*s1 == '*' && *s2 == '\0')
-			return wildcmp(++s1, s2);
+			return (wildcmp(++s1, s2));
 		return (0);
 	}
 	if (*s1 == *s2)
 	{
-		return wildcmp(++s1, ++s2);
+		return (wildcmp(++s1, ++s2));
 	}
 	else if (*s1 == '*')
 	{
 		if (*(s1 + 1) == '*')
-			return wildcmp(++s1, s2);
+			return (wildcmp(++s1, s2));
 		else
 		{
-			return wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) +s2);
+			return (wildcmp(s1, findsrc(s2, *(s1 + 1), 0, 0) + s2));
 		}
 	}
 	else if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-		       return wildcmp(s1, ++s2);
+		       return (wildcmp(s1, ++s2));
 		else
 		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
+			return (wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2));
 		}
 	}
 	return (0);
@@ -44,8 +45,8 @@ int wildcmp(char *s1, char *s2)
 int findsrc(char *s, char c, int i, int p)
 {
 	if (*(s +i) == '\0')
-		return (p +1);
-	else if (*(s +i) == c || *(s + i) == '*')
+		return (p + 1);
+	else if (*(s + i) == c || *(s + i) == '*')
 		p = i;
-	return (findsrc(s, c, i +1, p));
+	return (findsrc(s, c, i + 1, p));
 }
