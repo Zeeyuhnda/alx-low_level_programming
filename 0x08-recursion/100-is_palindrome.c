f03706224ac33ebbe0e1a0f70352e941cf0bdf81
Ziyanda <zeenkosi572@gmail.com>
2023-09-27 15:50:01 +0200

    Optional taks

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * is_palindrome - check if a string is a palindrome
- *   * @s: char array string
- *    * Return: 1 if palindrome, 0 if not
- *     */
+ * is_palindrome - check if a string is a palindrome
+ * @s: char array string
+ * Return: 1 if palindrome, 0 if not
+ */
 
 int is_palindrome(char *s)
 {
