90bfbde8ea65ebb4d96a843a6f831438af3df3ff
Ziyanda <zeenkosi572@gmail.com>
2023-09-29 13:35:06 +0200

    mistake

---
+++ 0x09-static_libraries/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - ad 2 positive num, print result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: if no num passed, print 0
+ *if a num has non-digits, print Error
+ * Return: 1 if error, 0 if function runs
+ */
+
+int main(int argc, char *argv[])
+{
+	int total, i;
+	char *p;
+	int num;
+
+	total = 0;
+	if (argc > 1)
+	{ 
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
