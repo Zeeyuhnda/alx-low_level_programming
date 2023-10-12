f09102490fd89ad0e410f298957d76fe54c9f977
Ziyanda <zeenkosi572@gmail.com>
2023-10-12 13:26:21 +0200

    task 3 changes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,10 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
+typedef struct print
+{
+		char *x;
+			void (*T_func)(va_list);
+} t_print;
+
 #endif
