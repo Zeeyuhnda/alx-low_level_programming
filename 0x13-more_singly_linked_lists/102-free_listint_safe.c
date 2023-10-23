92ea3f76f6430c0b451fe3891365a51433fa5190
Ziyanda <zeenkosi572@gmail.com>
2023-10-23 20:05:57 +0200

    Task 11

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,30 +1,70 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * free_listint_safe - free a `listint_t` list and set the head to null
- * @h: double pointer to head of linked list
- * Description: This function should work for circular lists
- * Only loop through the list once
- * Return: size of the list that was free'd
+ * add_nodeaddr - A function that mallocs a new space for a list
+ * @head: A double pointer that points to my second list header, head_2.
+ */
+addr_list *add_nodeaddr(addr_list **head, const void *addr)
+{
+	addr_list *new_node;
+
+	new_node = malloc(sizeof(addr_list));
+	if (new_node == NULL)
+	{
+		free_listaddr(*head);
+		exit (98);
+	}
+	new_node->addr = addr;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+/**
+ * free_listaddr - a function that frees a linked list
+ * @head: a pointer variable that points to the start of a newly made list
+ */
+void free_listaddr(addr_list *head)
+{
+	addr_list *current_node;
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node);
+	}
+}
+/**
+ * print_listint_safe - a function that prints the node and the
+ * number within a node,
+ * Return: the number of nodes
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *hold;
-	size_t count;
+	addr_list *current_NewNode;
+	addr_list *head_2;
+	size_t i;
 
-	count = 0;
-	current = *h;
-	while (current != NULL)
+	i = 0;
+	head_2 = NULL;
+	while (head != NULL)
 	{
-		count++;
-		hold = current;
-		current = current->next;
-		free(hold);
-
-	if (hold < current)
-		break;
+		current_NewNode = head_2;
+		while (current_NewNode != NULL)
+		{
+			if (head == current_NewNode->addr)
+			{
+				printf("(nil), (nil)");
+				free_listaddr(head_2);
+				return (i);
+			}
+			current_NewNode = current_NewNode->next;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		add_nodeaddr(&head_2, head);
+		head = head->next;
+		i++;
 	}
-	*h = NULL;
-
-	return (count);
+	free_listaddr(head_2);
+	return (i);
 }
